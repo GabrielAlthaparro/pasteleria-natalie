e25a7616c5e7b020b82785281d06c516a88331b3
Drobotij <drobotij.n@gmail.com>
2022-07-26 20:29:21 -0300

    Resolvi el problema que no dejaba conectarse a la base de datos

--- src/database.js
+++ src/database.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql');
 const host = 'sql10.freemysqlhosting.net';
 const user = 'sql10508968';
-const password = 'CX8HkVZ1f5.';
+const password = 'CX8HkVZ1f5';
 const database = 'sql10508968';
+const port = "3306"
 
 const conexion = mysql.createConnection({
     host,
     user,
     password,
-    database
+    database,
+    port
 });
 
 conexion.connect((err) => {

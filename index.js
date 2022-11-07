9f893fd487fa64285b18fe6ff094dd690364b8b1
Gabriel Althaparro <gabriel.altha@gmail.com>
2022-11-07 16:13:14 -0300

    Se hizo commit para recargar app en heroku, por supuesta vulnerabilidad

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 'use strict';
 const app = require('./src/app');
-app.listen(app.get('port'), () => console.log(`App listening on port ${app.get('port')}`));
+app.listen(app.get('port'), () => {
+  console.log(`App listening on port ${app.get('port')}`)
+});

8f8997bf37f6b4139bfca77ebdcd0b365eeb1388
Gabriel Althaparro <gabriel.altha@gmail.com>
2023-02-04 02:48:49 -0300

    Se cambiaron los charset y collate de la db para usar los mismos que en railway

--- config/db_base.sql
+++ config/db_base.sql
@@ -31,11 +31,11 @@ DROP TABLE IF EXISTS `imagenes`;
 CREATE TABLE IF NOT EXISTS `imagenes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_producto` int(11) NOT NULL,
-  `path` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+  `path` varchar(150) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `principal` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`),
   KEY `id_producto_ibfk` (`id_producto`)
-) ENGINE=InnoDB AUTO_INCREMENT=496623 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -45,9 +45,9 @@ CREATE TABLE IF NOT EXISTS `imagenes` (
 
 DROP TABLE IF EXISTS `invalid_tokens`;
 CREATE TABLE IF NOT EXISTS `invalid_tokens` (
-  `token` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `token` varchar(200) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`token`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -58,13 +58,13 @@ CREATE TABLE IF NOT EXISTS `invalid_tokens` (
 DROP TABLE IF EXISTS `mensajes`;
 CREATE TABLE IF NOT EXISTS `mensajes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
-  `nombre` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
-  `aclaraciones` text COLLATE utf8_spanish_ci,
-  `fecha` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(70) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nombre` varchar(70) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `aclaraciones` text COLLATE utf8mb4_0900_ai_ci,
+  `fecha` varchar(10) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `estado` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `mensajes_productos` (
   `cantidad` int(11) NOT NULL,
   PRIMARY KEY (`id_mensaje`,`id_producto`),
   KEY `id_producto` (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -90,13 +90,13 @@ CREATE TABLE IF NOT EXISTS `mensajes_productos` (
 DROP TABLE IF EXISTS `productos`;
 CREATE TABLE IF NOT EXISTS `productos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id_tipo` int(11) NOT NULL,
   `cantidad_consultas` int(11) NOT NULL DEFAULT '0',
-  `descripcion` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tipo_ibfk` (`id_tipo`)
-) ENGINE=InnoDB AUTO_INCREMENT=50363 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -107,9 +107,9 @@ CREATE TABLE IF NOT EXISTS `productos` (
 DROP TABLE IF EXISTS `tipos`;
 CREATE TABLE IF NOT EXISTS `tipos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` varchar(70) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `tipos`
@@ -128,13 +128,13 @@ INSERT INTO `tipos` (`id`, `descripcion`) VALUES
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `apellido` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_key` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `user`
@@ -152,9 +152,9 @@ INSERT INTO `user` (`id`, `email`, `nombre`, `apellido`, `password`) VALUES
 DROP TABLE IF EXISTS `user_tokens`;
 CREATE TABLE IF NOT EXISTS `user_tokens` (
   `id_user` int(11) NOT NULL,
-  `token` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id_user`,`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Restricciones para tablas volcadas
